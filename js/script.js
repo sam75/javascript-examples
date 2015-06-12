9490abeb6513be08b2a2032b736a5a7692c121ab
Sampath <sampath.ms@gmail.com>
2015-06-12 13:10:41 +0530

    Added the alert dialog

--- js/script.js
+++ js/script.js
@@ -6,3 +6,5 @@ Function.prototype.method = function (name, func){
 Number.method('integer', function (){
     return Math[this<0 ? 'ceil' : 'floor'](this);
 });
+
+alert(document.write((-10/3).integer( )));
